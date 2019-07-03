416fc710056f5412136901c0cac7362d6ff11b36
Edy Segura <edysegura@gmail.com>
2019-07-02 22:37:35 -0300

    Implement integration between product controller and service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Get } from "@nestjs/common";
 
 import { Product } from "./product.model";
 import { ProductsService } from "./products.service";
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
   add(@Body() product: Product): Product {
-    // this.productsService.insert();
-    product.id = String(Date.now())
-    return product;
+    const newProduct = this.productsService.create(product)
+    return newProduct;
+  }
+
+  @Get()
+  getAll() {
+    return this.productsService.list();
   }
 }
